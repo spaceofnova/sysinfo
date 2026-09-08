689cf6eaf0c48454a541259732a3616c55053bd9
Nova Space <mail.spaceofnova@gmail.com>
2026-09-08 02:29:19 -0500

    init server

---
+++ index.ts
@@ -0,0 +1,25 @@
+import os from "node:os";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.get("/sys", (req, res) => {
+  console.log(`${req.method} ${req.url} from ${req.ip}`)
+  res.send({
+    platform: os.platform(),
+    arch: os.arch(),
+    uptime: os.uptime(),
+    totalmem: os.totalmem(),
+    freemem: os.freemem(),
+    homedir: os.homedir(),
+    interfaces: os.networkInterfaces(),
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is up and running!");
+});
